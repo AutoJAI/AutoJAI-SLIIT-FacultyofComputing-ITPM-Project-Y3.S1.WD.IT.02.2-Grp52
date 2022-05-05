83476b2e14d3dbfb0bc3ddf25fb1dd3cd454d717
Vipuni <vipuniranasinghe@gmail.com>
2022-05-05 21:00:07 +0530

    Backend & Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Home from "./Components/Common/Home";
+
+import Login from "./Components/Common/Login";
+
+
+
+
+function App() {
+  return (
+    <div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+         
+          <Route path="/login" exact component={Login} />
+         
+          
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
