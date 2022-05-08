1b6fa1c110ebc80abd2486cbcb8df44fb22fa935
ghsahan <89076679+ghsahan@users.noreply.github.com>
2022-05-08 23:20:40 +0530

    Adding cutomer details

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,11 +1,11 @@
 const routes = require("express").Router();
-
+const CustomerMainRoute = require("./CustomerRoutes");
 const StoresMainRoute = require("./StoresRoutes");
 const PurchaseMainRoute = require("./PurchaseRoutes");
 const EmployeeMainRoute = require("./EmployeeRoutes");
 
 
-
+routes.use("/customer", CustomerMainRoute);
 
 routes.use("/stores", StoresMainRoute);
 
